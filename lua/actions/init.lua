b78239fb1097d220feba105c26af60f88d3d9ac8
mrtnvgr <martynovegorOF@yandex.ru>
2026-04-02 21:17:07 +0700

    feat(stock_actions): add word counters

--- lua/actions/init.lua
+++ lua/actions/init.lua
@@ -13,9 +13,17 @@ function M.get_actions()
 end
 
 M.stock_actions = {
-  ["LSP: Toggle inlay hints"] = function()
+  ["[LSP]: Toggle inlay hints"] = function()
     local enabled = vim.lsp.inlay_hint.is_enabled()
     vim.lsp.inlay_hint.enable(not enabled)
+  end,
+
+  ["[Buffer]: Show word count"] = function()
+    print("Total words: " .. vim.fn.wordcount().words)
+  end,
+
+  ["[Visual selection]: Show word count"] = function()
+    print("Words in visual selection: " .. vim.fn.wordcount().visual_words)
   end,
 };
 
